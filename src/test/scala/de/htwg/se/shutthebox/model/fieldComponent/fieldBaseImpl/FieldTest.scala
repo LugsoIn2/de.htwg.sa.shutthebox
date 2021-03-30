29ae2a180ed391ad8aaf98a8295c40e6197e752f
Andreas Ly <andreas.ly96@gmail.com>
2021-03-30 15:34:18 +0200

    Test Coverage BigField 95%

--- src/test/scala/de/htwg/se/shutthebox/model/fieldComponent/fieldBaseImpl/FieldTest.scala
+++ src/test/scala/de/htwg/se/shutthebox/model/fieldComponent/fieldBaseImpl/FieldTest.scala
@@ -1,39 +1,32 @@
 package de.htwg.se.shutthebox.model.fieldComponent.fieldBaseImpl
 
+import org.scalatest.Matchers.be
 import org.scalatest._
 
 class FieldTest extends WordSpec with Matchers {
 
-  val field = new Field()
-  /*
-  "A Field" should {
-    "a value" in {
-      field shouldBe a [Field]
-      field.field shouldBe a [Array[_]]
-      field.shut(1, field) shouldBe a [Cell]
-      field.toString shouldBe a [String]
-    }
-  }*/
-
   "A Field" when {
-    "is set an Array as parameter" should {
+    "is set with array with two cells as parameter" should {
       val field = Field(Array[Cell](Cell(), Cell()))
+      "have array size two" in {
+        field.field.length should be (2)
+      }
+    }
+
+    "is set without parameter" should {
+      val field = new Field()
       "have a array size nine" in {
-      field.field.length should be (2)
+      field.field.length should be (9)
       }
-      /*
-      "should be from type Field" in {
-        field should be [Field]
+      "have array field.field" in {
+        field.field shouldBe a [Array[Cell]]
       }
-      "field.field should be from type Array" in {
-        field.field should be [Array]
+      "have a cell in field.shut" in {
+        field.shut(1, field) shouldBe a [Cell]
       }
-      "field.shut(1. field) should be from type Cell" in {
-        field.shut(1, field) should be [Cell]
+      "have a string in field.toString" in {
+        field.toString shouldBe a [String]
       }
-      "field.toString should be from type String" in {
-        field.toString should be [String]
-      }*/
     }
   }
 
