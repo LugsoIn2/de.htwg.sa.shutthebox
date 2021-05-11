6a2a405facb032487ae0b4451bb27af667c2d098
Andreas Ly <andreas.ly96@gmail.com>
2021-05-11 16:36:49 +0200

    first db implementation

--- FileIO/src/main/scala/de/htwg/se/shutthebox/FileIOModule.scala
+++ FileIO/src/main/scala/de/htwg/se/shutthebox/FileIOModule.scala
@@ -1,6 +1,7 @@
 package de.htwg.se.shutthebox
 
 import com.google.inject.AbstractModule
+import de.htwg.se.shutthebox.model.DAOComponent.DAOMongoDBImp.FileIOMongoDAO
 import de.htwg.se.shutthebox.model.DAOComponent.DAObaseImpl.FileIODAO
 import de.htwg.se.shutthebox.model.DAOComponent.FileIODAOInterface
 import de.htwg.se.shutthebox.model.fileIoComponent.{FileIOInterface, fileIoJsonImpl, fileioDBImpl}
@@ -9,7 +10,7 @@ import net.codingwell.scalaguice.ScalaModule
 class FileIOModule extends AbstractModule with ScalaModule {
   override def configure() = {
     bind[FileIOInterface].to[fileioDBImpl.FileIO]
-    bind[FileIODAOInterface].toInstance(FileIODAO())
+    bind[FileIODAOInterface].toInstance(FileIOMongoDAO())
 }
 
 }
--- FileIO/src/main/scala/de/htwg/se/shutthebox/model/DAOComponent/DAOMongoDBImp/FileIOMongoDAO.scala
+++ FileIO/src/main/scala/de/htwg/se/shutthebox/model/DAOComponent/DAOMongoDBImp/FileIOMongoDAO.scala
@@ -13,18 +13,32 @@ case class FileIOMongoDAO() extends FileIODAOInterface {
   implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "my-system")
   implicit val executionContext: ExecutionContextExecutor = system.executionContext
 
-  val uri: String = "mongodb+srv://<username>:<password>@<cluster-address>/test?retryWrites=true&w=majority"
-  System.setProperty("org.mongodb.async.type", "netty")
+  val uri: String = "mongodb://localhost:27017/test?retryWrites=true&w=majority"
+  //System.setProperty("org.mongodb.async.type", "netty")
   val client: MongoClient = MongoClient(uri)
   val db: MongoDatabase = client.getDatabase("test")
 
+  override def create(field: JsValue): Unit = {
+    println("FILEIOMONGOCREATE")
+    val collection : MongoCollection[Document] = db.getCollection("test")
+    val doc: Document = Document("_id" -> 0, "field" -> List(true, true))
+    //collection.insertOne(doc)
+    val observable: Observable[Completed] = collection.insertOne(doc)
 
-
-  override def create(field: JsValue): Unit = ???
+    observable.subscribe(new Observer[Completed] {
+      override def onNext(result: Completed): Unit = println(s"onNext: $result")
+      override def onError(e: Throwable): Unit = println(s"onError: $e")
+      override def onComplete(): Unit = println("onComplete")
+    })
+  }
 
   override def read(): String = ???
 
   override def update(): Unit = ???
 
   override def delete(): Unit = ???
+
+
+
+
 }
