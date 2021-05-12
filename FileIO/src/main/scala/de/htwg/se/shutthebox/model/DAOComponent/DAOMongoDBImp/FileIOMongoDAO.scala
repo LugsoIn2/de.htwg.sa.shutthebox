c7430bf53c9fe401777464eb707b4f68c6ae3111
LugsoIn2 <timmy.pfau@netpy.de>
2021-05-12 09:10:19 +0200

    removed comments

--- FileIO/src/main/scala/de/htwg/se/shutthebox/model/DAOComponent/DAOMongoDBImp/FileIOMongoDAO.scala
+++ FileIO/src/main/scala/de/htwg/se/shutthebox/model/DAOComponent/DAOMongoDBImp/FileIOMongoDAO.scala
@@ -19,7 +19,6 @@ case class FileIOMongoDAO() extends FileIODAOInterface {
   implicit val executionContext: ExecutionContextExecutor = system.executionContext
 
   val uri: String = "mongodb://localhost:27017/test?retryWrites=true&w=majority"
-  //System.setProperty("org.mongodb.async.type", "netty")
   val client: MongoClient = MongoClient(uri)
   val db: MongoDatabase = client.getDatabase("fileIoDatabase")
   val gameCollection : MongoCollection[Document] = db.getCollection("game")
@@ -27,15 +26,11 @@ case class FileIOMongoDAO() extends FileIODAOInterface {
   var id = 0
 
   override def create(json: JsValue): Unit = {
-    //create one Dummy werten
-    //delete()
     val IDDoc : Document = Document("_id" -> id.toString, "id" -> id)
     observableHandler(IDCollection.insertOne(IDDoc))
 
     val idDoc = Await.result(IDCollection.find(equal("_id", id.toString)).first().head(), atMost = 10.second).toJson()
-    //val tmp = Json.toJson(idDoc)
     println("idDoc" + idDoc)
-    //println("tmp" + tmp)
     val tmp = Json.parse(idDoc)
     val oldId = (tmp \ "id").as[Int]
     println("oldID" + oldId)
@@ -46,12 +41,7 @@ case class FileIOMongoDAO() extends FileIODAOInterface {
     val field = (json \ "field").as[Array[Boolean]].toList
     val fieldDoc : Document = Document("_id" -> newId.toString, "field" -> field)
 
-    val dices = (json \ "dice").as[JsObject]
-    //val dicesDoc : Document = Document("_id" -> "diceDoc", "dices" -> List((dices \ "die1").as[Int],(dices \ "die2").as[Int]))
-
     observableHandler(gameCollection.insertOne(fieldDoc))
-    //observableHandler(gameCollection.insertOne(dicesDoc))
-    //IDCollection.replaceOne(equal("_id", "IDDoc"),  Document("id" -> newid))
   }
 
   override def read(): String = {
